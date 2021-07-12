263bc99215e81f57033803670cb9b0a669bcedf0
F.  Salvo <68975104+fhsal@users.noreply.github.com>
2021-07-11 22:35:41 -0400

    Add files via upload

--- flatten.js
+++ flatten.js
@@ -5,6 +5,22 @@ let e = {...a, ...b}
 
 let epicFlatArray = [];
 
+function epicFlatten4 (obj){
+    {for (i=0; i<(obj.length-1); i++){
+        console.log ('i= ' + i)
+        if(obj[i][1]){
+          obj[i][1].forEach(element => {
+              let fullEpic = { ...obj[i][0], ...element}
+          epicFlatArray.push(fullEpic)    
+          });
+      }
+    }
+  
+  }
+  }
+
+
+
 function epicFlatten (obj){
   {for (i=0; i<(5); i++){
       console.log ('i= ' + i)
